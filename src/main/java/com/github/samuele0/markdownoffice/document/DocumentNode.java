e70de245dacb40dc87c012a84ff01b1eff810bab
samuele0 <samuele0@tutanota.com>
2020-01-06 13:45:34 +0100

    Forgot to commit a file :(

--- src/main/java/com/github/samuele0/markdownoffice/document/DocumentNode.java
+++ src/main/java/com/github/samuele0/markdownoffice/document/DocumentNode.java
@@ -1,4 +1,26 @@
 package com.github.samuele0.markdownoffice.document;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public abstract class DocumentNode {
+    private List<DocumentNode> childs;
+    private DocumentNode parent;
+
+    public DocumentNode() {
+        childs = new LinkedList<>();
+    }
+
+    public void addChild(DocumentNode node) {
+        childs.add(node);
+        node.setParent(this);
+    }
+
+    public DocumentNode getParent() {
+        return parent;
+    }
+
+    public void setParent(DocumentNode parent) {
+        this.parent = parent;
+    }
 }
