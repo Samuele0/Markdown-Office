78d4dd0c767ffe5bfaae7a194e462b37da5762cc
samuele0 <samuele0@tutanota.com>
2020-01-11 15:23:01 +0100

    implemented plugin System

--- src/main/java/com/github/samuele0/markdownoffice/MyController.java
+++ src/main/java/com/github/samuele0/markdownoffice/MyController.java
@@ -2,19 +2,21 @@ package com.github.samuele0.markdownoffice;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import github.samuele0.plugins.Plugin;
 import javafx.fxml.Initializable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class MyController implements Initializable {
     Logger log = LogManager.getLogger();
 
     @Inject
-    public MyController(@Named("application.hellomessage") String s) {
-        log.error(s);
+    public MyController(Set<Plugin> plugins) {
+        log.info(plugins.stream().findFirst().get().getClass().getCanonicalName());
     }
 
     @Override
--- src/main/java/com/github/samuele0/markdownoffice/MyFxmlLoader.java
+++ src/main/java/com/github/samuele0/markdownoffice/MyFxmlLoader.java
@@ -1,10 +1,9 @@
 package com.github.samuele0.markdownoffice;
 
-import com.google.inject.Binder;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import com.google.inject.*;
 import com.google.inject.Module;
 import com.google.inject.name.Names;
+import github.samuele0.plugins.Plugin;
 import javafx.fxml.FXMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,9 +12,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Properties;
-import java.util.PropertyResourceBundle;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MyFxmlLoader extends FXMLLoader {
     public MyFxmlLoader(URL location, ResourceBundle resources) {
@@ -34,21 +32,27 @@ public class MyFxmlLoader extends FXMLLoader {
         }
 
         private void bindConfiguration(Binder binder) {
-            Properties properties= new Properties();
+            Properties properties = new Properties();
             configureDefaultProperties(properties);
             try {
-                log.debug("Reading configuration File: "+Paths.get("../config.properties").toAbsolutePath().toString());
+                log.debug("Reading configuration File: " + Paths.get("../config.properties").toAbsolutePath().toString());
                 properties.load(Files.newBufferedReader(Paths.get("../config.properties")));
             } catch (IOException e) {
                 log.info("No configuration file found");
                 log.debug(e);
             }
 
-            Names.bindProperties(binder,properties);
+            Names.bindProperties(binder, properties);
+        }
+
+        @Provides
+        public Set<Plugin> getPlugins() {
+            return ServiceLoader.load(Plugin.class).stream().map(ServiceLoader.Provider::get).collect(Collectors.toSet());
+
         }
 
         private void configureDefaultProperties(Properties properties) {
-            properties.setProperty("application.hellomessage","Default Message");
+            properties.setProperty("application.hellomessage", "Default Message");
         }
     }
 }
